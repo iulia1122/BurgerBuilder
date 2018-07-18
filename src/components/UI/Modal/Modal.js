f17bebe86504f3c3c6c228aab313ddcd708dc30a
iulia <nedelcuniulia@gmail.com>
2018-07-18 20:32:31 +0100

    BurgerBuilder - Order Summary.

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import classes from './Modal.css'
-import Aux from '../../../hoc/Auxiliary/Aux'
+import Aux from '../../../hoc/Auxiliary/Auxiliary'
 import Backdrop from '../Backdrop/Backdrop'
 
 const modal = (props) => {
